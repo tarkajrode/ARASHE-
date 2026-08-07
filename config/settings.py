ed715a89c0b42660a23b7c56879338caf1620eb5
tarkajrode <----GitHub>
2026-08-08 00:50:29 +0300

    Allow Render production host

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dev-only-change-me')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.trycloudflare.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.trycloudflare.com', 'arashe-the-blood.onrender.com']
 
 
 # Application definition
